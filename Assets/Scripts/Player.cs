e94bf1d8196ed1df89c537d2a4e13457f885439f
Melissa Illig <melissaillig@Melissas-MBP.fritz.box>
2021-04-11 15:42:38 +0200

    update player Script to move the Player around

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
+public Transform thisTransform;
 public Vector3 mousePos;
 public Vector3 mouseCameraPos;
+public Vector3 playerPos;
+public float speed = 0.1f;
+
+
+    void Start() 
+    {
+      playerPos = thisTransform.position;
+    }
 
     void Update()
     {
-       if (Input.GetMouseButton(0)) {
+       if (Input.GetMouseButton(0))
+        {
              mousePos = Input.mousePosition;
-              mouseCameraPos = Camera.main.ScreenToWorldPoint(mousePos);
-                this.transform.position = mouseCameraPos;
+             mouseCameraPos = Camera.main.ScreenToWorldPoint(mousePos);
+             playerPos = new Vector3(mouseCameraPos.x, mouseCameraPos.y, thisTransform.position.z);
+        } 
 
-       } 
+        thisTransform.position = Vector3.MoveTowards(thisTransform.position, playerPos, speed);
     }
 }
